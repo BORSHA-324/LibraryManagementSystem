316313b757ab8c653e30e61ca91454e00dcea617
priomaDas <dasprioma6@gmail.com>
2026-04-19 22:24:57 +0600

    Update Borrowing class to support Memento pattern

--- Borrowing.java
+++ Borrowing.java
@@ -1,34 +1,30 @@
-
 package models;
 
 import java.io.Serializable;
 import java.util.Date;
+import models.memento.BorrowingMemento;
 
-public class Borrowing implements Serializable 
-{
-    
-    // ENUM INSIDE CLASS
-    public enum BorrowingStatus 
-    {
+public class Borrowing implements Serializable {
+
+   
+    public enum BorrowingStatus {
         BORROWED("Borrowed"),
         RETURNED("Returned"),
         OVERDUE("Overdue");
-        
+
         private final String displayName;
-        
-        BorrowingStatus(String displayName) 
-        {
+
+        BorrowingStatus(String displayName) {
             this.displayName = displayName;
         }
-        
+
         @Override
-        public String toString() 
-        {
+        public String toString() {
             return displayName;
         }
     }
+
     
-    // Borrowing attributes
     private String borrowingId;
     private String bookId;
     private String memberId;
@@ -36,15 +32,13 @@ public class Borrowing implements Serializable
     private Date dueDate;
     private Date returnDate;
     private BorrowingStatus status;
+
     
-    // Default constructor
-    public Borrowing() 
-    {
-        
+    public Borrowing() {
     }
-    
-    // Parameterized constructor
-    public Borrowing(String borrowingId, String bookId, String memberId,Date borrowDate, Date dueDate, Date returnDate, BorrowingStatus status) {
+    public Borrowing(String borrowingId, String bookId, String memberId,
+                     Date borrowDate, Date dueDate,
+                     Date returnDate, BorrowingStatus status) {
         this.borrowingId = borrowingId;
         this.bookId = bookId;
         this.memberId = memberId;
@@ -53,65 +47,83 @@ public class Borrowing implements Serializable
         this.returnDate = returnDate;
         this.status = status;
     }
-    
-    // Getters
-    public String getBorrowingId() 
-    { 
-        return borrowingId; 
+
+    public String getBorrowingId() {
+        return borrowingId;
     }
-    public String getBookId() 
-    { 
-        return bookId; 
+
+    public String getBookId() {
+        return bookId;
     }
-    public String getMemberId() 
-    { 
-        return memberId; 
+
+    public String getMemberId() {
+        return memberId;
     }
-    public Date getBorrowDate() 
-    { 
-        return borrowDate; 
+
+    public Date getBorrowDate() {
+        return borrowDate;
     }
-    public Date getDueDate() 
-    { 
-        return dueDate; 
+
+    public Date getDueDate() {
+        return dueDate;
     }
-    public Date getReturnDate() 
-    { 
-        return returnDate; 
+
+    public Date getReturnDate() {
+        return returnDate;
     }
-    public BorrowingStatus getStatus() 
-    { 
-        return status; 
+
+    public BorrowingStatus getStatus() {
+        return status;
     }
-    
-    // Setters
-    public void setBorrowingId(String borrowingId) 
-    { 
-        this.borrowingId = borrowingId; 
+
+    public void setBorrowingId(String borrowingId) {
+        this.borrowingId = borrowingId;
     }
-    public void setBookId(String bookId) 
-    { 
-        this.bookId = bookId; 
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
     }
-    public void setMemberId(String memberId) 
-    { 
-        this.memberId = memberId; 
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
     }
-    public void setBorrowDate(Date borrowDate) 
-    { 
-        this.borrowDate = borrowDate; 
+
+    public void setBorrowDate(Date borrowDate) {
+        this.borrowDate = borrowDate;
     }
-    public void setDueDate(Date dueDate) 
-    { 
-        this.dueDate = dueDate; 
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
     }
-    public void setReturnDate(Date returnDate) 
-    { 
-        this.returnDate = returnDate; 
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
     }
-    public void setStatus(BorrowingStatus status) 
-    { 
-        this.status = status; 
+
+    public void setStatus(BorrowingStatus status) {
+        this.status = status;
+    }
+
+ 
+    public BorrowingMemento save() {
+        return new BorrowingMemento(
+                borrowingId,
+                bookId,
+                memberId,
+                borrowDate,
+                dueDate,
+                returnDate,
+                status
+        );
+    }
+
+    public void restore(BorrowingMemento memento) {
+        this.borrowingId = memento.getBorrowingId();
+        this.bookId = memento.getBookId();
+        this.memberId = memento.getMemberId();
+        this.borrowDate = memento.getBorrowDate();
+        this.dueDate = memento.getDueDate();
+        this.returnDate = memento.getReturnDate();
+        this.status = memento.getStatus();
     }
 }
-
