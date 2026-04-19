75986e7bee306a7e676d378782a09540534cc0f4
priomaDas <dasprioma6@gmail.com>
2026-04-19 22:26:51 +0600

    Update Member class to support Memento pattern

--- Member.java
+++ Member.java
@@ -1,32 +1,30 @@
-
 package models;
 
 import java.io.Serializable;
 import java.util.Date;
+import models.memento.MemberMemento;
 
 public class Member implements Serializable {
-    
-    
-    public enum MembershipType 
-    {
+
+    public enum MembershipType {
         STUDENT("Student"),
         TEACHER("Teacher"),
         PUBLIC("Public"),
         SENIOR("Senior Citizen");
-        
+
         private final String displayName;
-        
+
         MembershipType(String displayName) {
             this.displayName = displayName;
         }
-        
+
         @Override
         public String toString() {
             return displayName;
         }
     }
-    
-    // Member attributes
+
+
     private String memberId;
     private String name;
     private String email;
@@ -34,14 +32,13 @@ public class Member implements Serializable {
     private MembershipType membershipType;
     private Date joinDate;
     private String address;
-    
-    // Default constructor
+
+
     public Member() {
     }
-    
-    // Parameterized constructor
-    public Member(String memberId, String name, String email, String phone,MembershipType membershipType, Date joinDate, String address) 
-    {
+
+    public Member(String memberId, String name, String email, String phone,
+                  MembershipType membershipType, Date joinDate, String address) {
         this.memberId = memberId;
         this.name = name;
         this.email = email;
@@ -50,65 +47,84 @@ public class Member implements Serializable {
         this.joinDate = joinDate;
         this.address = address;
     }
-    
-    // Getters
-    public String getMemberId() 
-    { 
-        return memberId; 
+
+ 
+    public String getMemberId() {
+        return memberId;
     }
-    public String getName() 
-    { 
-        return name; 
+
+    public String getName() {
+        return name;
     }
-    public String getEmail() 
-    { 
-        return email; 
+
+    public String getEmail() {
+        return email;
     }
-    public String getPhone() 
-    { 
-        return phone; 
+
+    public String getPhone() {
+        return phone;
     }
-    public MembershipType getMembershipType() 
-    { 
-        return membershipType; 
+
+    public MembershipType getMembershipType() {
+        return membershipType;
     }
-    public Date getJoinDate() 
-    { 
-        return joinDate; 
+
+    public Date getJoinDate() {
+        return joinDate;
     }
-    public String getAddress() 
-    { 
-        return address; 
+
+    public String getAddress() {
+        return address;
     }
-    
-    // Setters
-    public void setMemberId(String memberId) 
-    { 
-        this.memberId = memberId; 
+
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
     }
-    public void setName(String name) 
-    { 
-        this.name = name; 
+
+    public void setName(String name) {
+        this.name = name;
     }
-    public void setEmail(String email) 
-    { 
-        this.email = email; 
+
+    public void setEmail(String email) {
+        this.email = email;
     }
-    public void setPhone(String phone) 
-    { 
-        this.phone = phone; 
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
-    public void setMembershipType(MembershipType membershipType) 
-    { 
-        this.membershipType = membershipType; 
+
+    public void setMembershipType(MembershipType membershipType) {
+        this.membershipType = membershipType;
     }
-    public void setJoinDate(Date joinDate) 
-    { 
-        this.joinDate = joinDate; 
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
     }
-    public void setAddress(String address) 
-    { 
-        this.address = address; 
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public MemberMemento save() {
+        return new MemberMemento(
+                memberId,
+                name,
+                email,
+                phone,
+                membershipType,
+                joinDate,
+                address
+        );
+    }
+
+    public void restore(MemberMemento memento) {
+        this.memberId = memento.getMemberId();
+        this.name = memento.getName();
+        this.email = memento.getEmail();
+        this.phone = memento.getPhone();
+        this.membershipType = memento.getMembershipType();
+        this.joinDate = memento.getJoinDate();
+        this.address = memento.getAddress();
     }
 }
-
